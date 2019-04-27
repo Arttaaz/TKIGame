3134bd35cdefd135d898ee663417565f7dfd0e85
Amaury Butaux <ymarua@hotmail.fr>
2019-04-27 13:19:40 +0200

    olala

--- unit.py
+++ unit.py
@@ -8,14 +8,19 @@ from gameobject import GameObject
 class State(Enum):
     IDLE = 1
     SHOOT = 2
-    
-    
+    DEAD = 3
+bullet_time = 0.3
+
 class Unit(GameObject):
 
     def __init__(self, image, id = 1, collide = True):
         GameObject.__init__(self, image, id, collide)
         self.state = State.SHOOT
         self.target = None
+        self.hp = 100
+        self.bullet_progress = 0
+        self.start_shooting_time = 0
+        self.start_shooting = False
     def follow(self, target):
         pass
 
@@ -34,10 +39,38 @@ class Unit(GameObject):
 
         if self.state == State.SHOOT and self.target is not None:
             self.rotation = 0.9 * self.rotation + 0.1 * self.rotation_to_target()
+            self.bullet_progress += 1 / 60 / (bullet_time)
 
+            if self.bullet_progress > 1:
+                self.bullet_progress = 0
+                self.target.hp -= 10
+        if self.hp < 0:
+            self.state = State.DEAD
+            self.rotation += 1
+            
     def draw(self, screen, x, y):
         super().draw(screen, x, y)
 
-        #if self.state == State.SHOOT and self.target is not None:
-            #pygame.draw.aalines(screen, (0, 0, 0), (self.rect.centerx, self.rect.centery)
-    
+        if self.state == State.SHOOT and self.target is not None:
+            pos1 = pygame.Vector2(self.rect.centerx, self.rect.centery)
+            pos2 = pygame.Vector2(self.target.rect.centerx, self.target.rect.centery)
+            dir = (pos2 - pos1).normalize()
+            begin = pos1 +  dir * 23
+            end = pos2 - dir * 11
+
+            if self.bullet_progress < 0.1:
+                p1 = begin
+            else:
+                t = (self.bullet_progress - 0.1) / 0.9
+                p1 = (1 - t) * begin + t * end
+            if self.bullet_progress > 0.9:
+                p2 = end
+            else:
+                t = self.bullet_progress / 0.9
+                p2 = (1 - t) * begin + t * end
+            
+            
+            pygame.draw.aaline(screen, (0, 0, 0), (p1.x, p1.y),
+                                (p2.x, p2.y))
+
+
