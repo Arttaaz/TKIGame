779c755395588b856f98c3166aef3ecf5f96f3c2
Pierre PEREIRA <PostMortem@pcpostmortem>
2019-05-02 19:08:14 +0200

    commentaires

--- gameobject.py
+++ gameobject.py
@@ -1,22 +1,42 @@
-import pygame
-class GameObject:
+"""
+Objet représenté sur la carte.
+C'est un objet parent, dont d'autres
+objets peuvent hériter pour ajouter
+des fonctionnalités (dont l'objet 'Unit')
+"""
 
+
+import pygame
+
+
+class GameObject:
     def __init__(self, image, grid, xmap, ymap, id_obj=0, collide=False):
-        self.image = image
-        self.image_to_draw = image
+        self.image = image # image représentant l'objet sur la map
+        self.image_to_draw = image # image_to_draw subit des modifications de l'image avant d'être dessinée
         self.grid = grid
         self.xmap = xmap
         self.ymap = ymap
-        self.collide = collide
-        self.rotation = 0
+        self.collide = collide # est-ce que l'objet est de type bloquant ?
+        self.rotation = 0 # tourne l'image dans le bon sens
         self.rect = image.get_rect()
         self.id = id_obj
-        self.last_rotation = 0
+        self.last_rotation = 0 # permet d'éviter de tourner deux fois l'image alors qu'on en a pas besoin
 
     def update(self):
+        """
+        Fonction qu'on par défaut tout les objets.
+        On la créé là pour ne pas créer d'erreurs
+        lorsqu'on appel sans réfléchir cette méthode (car
+        certains objets n'ont pas besoin de update).
+        """
         pass
 
     def draw(self, screen, x, y):
+        """
+        Dessine l'objet sur la carte,
+        effectue une rotation de l'image avant,
+        si nécéssaire.
+        """
         if self.last_rotation != self.rotation:
             self.last_rotation = self.rotation
             self.image_to_draw = pygame.transform.rotate(self.image, self.rotation)
